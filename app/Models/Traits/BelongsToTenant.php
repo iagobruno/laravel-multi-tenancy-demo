e761785692e64f1f69a0108de5f3f1cb135275d6
Iago Bruno <iagobruno.dev@gmail.com>
2022-12-04 15:02:22 -0300

    refactor: Improvements on BelongsToTenent trait

--- app/Models/Traits/BelongsToTenant.php
+++ app/Models/Traits/BelongsToTenant.php
@@ -2,20 +2,30 @@
 
 namespace App\Models\Traits;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 trait BelongsToTenant
 {
+    public function tenant(): BelongsTo
+    {
+        return $this->belongsTo(\App\Models\Tenant::class);
+    }
+
     protected static function bootBelongsToTenant()
     {
-        static::creating(function ($user) {
-            if ($tenant = tenant()) {
-                $user->tenant_id = $tenant->id;
+        static::creating(function (Model $model) {
+            if (
+                !is_null(tenant()) &&
+                !$model->isDirty('tenant_id')
+            ) {
+                $model->forceFill(['tenant_id' => tenant()->id]);
             }
         });
 
-        static::addGlobalScope('tenant', function (Builder $builder) {
-            $builder->where('tenant_id', tenant()->id);
+        static::addGlobalScope('tenant', function (Builder $query) {
+            $query->whereBelongsTo(tenant());
         });
     }
 }
