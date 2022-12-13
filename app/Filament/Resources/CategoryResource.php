c4d31b5b324a119b15dc4364988d89336194d238
Iago Bruno <iagobruno.dev@gmail.com>
2022-12-13 17:54:01 -0300

    Add CategoryResource to dashboard

---
+++ app/Filament/Resources/CategoryResource.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CategoryResource\Pages;
+use App\Filament\Resources\CategoryResource\RelationManagers;
+use App\Models\Category;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Actions\{DeleteAction, EditAction, DeleteBulkAction};
+use Filament\Tables\Columns\{TextColumn};
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CategoryResource extends Resource
+{
+    protected static ?string $model = Category::class;
+
+    protected static ?string $slug = 'blog/categories';
+    protected static ?string $modelLabel = 'Categoria';
+    protected static ?string $pluralModelLabel = 'Categorias';
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?int $navigationSort = 1;
+    protected static ?string $navigationGroup = 'Blog';
+    protected static ?string $recordTitleAttribute = 'name';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->label('Nome da categoria:')
+                    ->required()
+                    ->maxLength(255)
+                    ->reactive()
+                    ->afterStateUpdated(fn ($state, callable $set) => $set('slug', str()->slug($state))),
+                TextInput::make('slug')
+                    ->label('Slug:')
+                    ->disabled()
+                    ->required()
+                    ->maxLength(255)
+                    ->unique(Category::class, 'slug', fn ($record) => $record),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('Nome')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('slug'),
+                TextColumn::make('created_at')
+                    ->label('Criado em')
+                    ->dateTime('d/m/Y à\s H:i')
+                    ->size('sm')
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->label('Modificado')
+                    ->since()
+                    ->size('sm')
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: false),
+            ])
+            ->defaultSort('name', 'ASC')
+            ->filters([
+                //
+            ])
+            ->actions([
+                EditAction::make()
+                    ->label('Editar'),
+                DeleteAction::make()
+                    ->label('Deletar'),
+            ])
+            ->bulkActions([
+                DeleteBulkAction::make()
+                    ->label('Deletar selecionados'),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategories::route('/'),
+        ];
+    }
+}
